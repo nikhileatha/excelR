cc4b6ce967300c0a5a818f649cd372446e279e99
nikhileatha <136676963+nikhileatha@users.noreply.github.com>
2025-01-03 12:29:34 +0530

    Add files via upload

---
+++ core java/demo2.java
@@ -0,0 +1,16 @@
+class Demo1{
+    public static void main(String []args){
+        int a=10;
+        int b=20;
+        if(a>b) {
+            system.out.println(x:"a is greater than b")
+        }
+        else if(a=b){
+            system.out.println(x:"a is equals to b")
+        }
+        else{
+            system.out.println(x:"a is less than b")
+        }
+        
+    }
+}
